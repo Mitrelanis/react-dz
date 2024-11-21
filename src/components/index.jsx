1f4ed03fead5b64b1988e8586d3d4cea228586bb
Mitrelanis <gashikulin_r@auca.kg>
2024-11-21 21:14:31 +0600

    1

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -6,9 +6,10 @@ export function ProductCard(props){
     return (<div className="product-card">
    
         <img src={image} alt="" className="image"/>
- 
+        <div className="category-name">
         <div className="category">{category}</div>
         <h4>{title}</h4>
+        </div>
         <div className="pricediv">
             <div className="price">${price}</div>
             <button>Add to card</button>
